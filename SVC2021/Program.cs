bc40e1b044cdcd79e86f59ac6335455c638a1766
Bence Kovari <kovari.bence@vik.bme.hu>
2021-03-01 13:56:19 +0100

    Added solution

---
+++ SVC2021/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace SVC2021
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // TASK: Get the Loader running
+            // TASK: Load comparison files and output predictions
+            // TASK: Create a verifier/classifier for the competition
+        }
+    }
+}
